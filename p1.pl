953d0f0a61b992b8d195d24deb787b5d69d4823d
herince <rherince@gmail.com>
2017-10-10 04:47:05 +0300

    p2

--- p1.pl
+++ p1.pl
@@ -1,15 +1,15 @@
-my_last(X, [X | []]) .
-my_last(X, [_ | L]) :- my_last(X, L) .
+last_element(X, [X ]) .
+last_element(X, [_ | L]) :- last_element(X, L) .
 
 :- begin_tests(tests).
 
-test(my_last__simple, all(Last == [3])) :-
-        my_last(Last, [1, 2, 3]) .
+test(last_element__simple, all(Last == [3])) :-
+        last_element(Last, [1, 2, 3]) .
 
-test(my_last__one_element, all(Last == [4])) :-
-        my_last(Last, [4]) .
+test(last_element__one_element, all(Last == [4])) :-
+        last_element(Last, [4]) .
         
-test(my_last__empty_list, fail) :-
-        my_last(_, []) .
+test(last_element__empty_list, fail) :-
+        last_element(_, []) .
 
 :- end_tests(tests).
